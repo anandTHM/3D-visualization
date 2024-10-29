9253132ea09ead93331d7e4c7d025556a158a22c
anand6404015 <anand@monktechlabs.com>
2024-10-29 15:07:28 +0530

    added

--- vite.config.js
+++ vite.config.js
@@ -11,15 +11,32 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
   plugins: [react()],
   build: {
     outDir: 'build',
-    chunkSizeWarningLimit: 1000,
-    // You might also want to add this to ensure the build directory is cleaned before each build
-    emptyOutDir: true
+    emptyOutDir: true,
+    chunkSizeWarningLimit: 1500, // Increased limit
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // Separate vendor chunks
+          'vendor-react': ['react', 'react-dom', 'react-router-dom'],
+          'vendor-ui': ['@mui/material', '@mui/icons-material'], // if you're using Material UI
+          'vendor-utils': ['lodash', 'axios'], // other large dependencies
+        },
+      },
+    },
+    // Ensure the build directory is created
+    assetsDir: 'assets',
+    minify: 'esbuild',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   }
 })
