ea8032873e875f67438866fc6809c11b24d5369d
anand6404015 <anand@monktechlabs.com>
2024-10-29 15:14:46 +0530

    added 2

--- vite.config.js
+++ vite.config.js
@@ -11,16 +11,9 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
   plugins: [react()],
-  build: {
-    outDir: './build', // Make sure path is explicit
-    emptyOutDir: true,
-    chunkSizeWarningLimit: 1500,
-    assetsDir: 'assets',
-    minify: 'esbuild'
-  }
 })
